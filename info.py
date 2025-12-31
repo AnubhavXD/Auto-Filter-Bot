5abacc148431918a986921bea69d62dc62b426cb
Anubhav <132827423+AnubhavXD@users.noreply.github.com>
2025-12-31 11:47:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -115,7 +115,7 @@ SEASONS = ["s01" , "s02" , "s03" , "s04", "s05" , "s06" , "s07" , "s08" , "s09" 
 STREAM_MODE = bool(environ.get('STREAM_MODE', False))
 
 NO_PORT = bool(environ.get('NO_PORT', False))
-APP_NAME = 'hermione-auto-filter-920a5a685465'
+APP_NAME = getenv("APP_NAME", "hermione-auto-filter-920a5a685465")
 if 'DYNO' in environ:
     ON_HEROKU = True
     APP_NAME = environ.get(APP_NAME)
